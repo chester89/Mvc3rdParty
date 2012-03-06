efc029c553a482e225401510c57306f2482fb1d1
Chermennov Gleb <thebitterend@gmail.com>
2012-03-06 11:08:05 +0400

    updated jQuery, added jQuery calendar support

--- Mvc3rdParty/Models/LoginModel.cs
+++ Mvc3rdParty/Models/LoginModel.cs
@@ -9,5 +9,6 @@ namespace Mvc3rdParty.Web.Models
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public DateTime? Edited { get; set; }
     }
 }
